176731d03bc618b1e7b5328c926a46049a09f669
Amine Kella <akella@e3r7p15.1337.ma>
2025-11-10 20:52:15 +0100

    done

--- server/Server.hpp
+++ server/Server.hpp
@@ -14,18 +14,16 @@ class Client;
 class Server
 {
     private:
-    // std::string             lineCmd;
+
     int						port;
     int						serverId;
     std::string             password;
     bool                    isGetSignal;
-    std::string _serverName;
-
     socklen_t addr_len;
-    sockaddr_in  serverConfig;//to bind the server fd and client //this contain
-    
+    sockaddr_in  serverConfig;
     std::vector<struct pollfd> poll_fds;
     std::map<int, Client>	ClientsInfo;
+
     int ReadClientMessage(std::string &line);
     void AddClient();
     void GetClientEvents();
@@ -33,10 +31,14 @@ class Server
     void waitConnection();
     void ConfigureSocket();
     void bindSocket();
-    std::string get_server_name();
     void setupServer();
     int stringToPort(std::string &string);
     void ListenSocket();
+    void ParseCmd(Client &client);
+	void PassCmd(Client &client, std::string password_arg);
+	void NickCmd();
+	void UserCmd();
+    std::vector<std::string> splitCmd(std::string &str);
     public:
         void  StartServer();
         Server(void);
@@ -45,8 +47,9 @@ class Server
         Server &operator=(const Server &other);
         void  join(std::vector<std::string> cmds, Client *c);
 		std::map<std::string,Channel*> channel;
-        // void quite_from_all_channel(Client *c , Channel *ch);
+
         ~Server();
 };
 
 #endif
+	

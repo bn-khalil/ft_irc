53907ebe3b642bd207870c6101cdc833cd76e7ef
bn-khalil <benkhaliltouda3201@gmail.com>
2025-11-24 19:36:00 +0100

    chore: handle some missing parts

--- server/Server.cpp
+++ server/Server.cpp
@@ -152,8 +152,8 @@ void Server::AddClient()
     poll_fds.push_back(ClientPollfd);
 
     Client client(ClientSocketFd);
-    ClientsInfo[ClientSocketFd] = client;
     client.set_hostname(hostname);
+    ClientsInfo[ClientSocketFd] = client;
     //need to add username
     std::cout << "client number " << ClientSocketFd << " connect" << std::endl;
 }
